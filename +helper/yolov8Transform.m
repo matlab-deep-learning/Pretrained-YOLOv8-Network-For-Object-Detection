509bd62da305cf30adcb6ce6849c0e385529c43d
vivek akkala <19205408+viakkala@users.noreply.github.com>
2024-02-29 00:10:43 +0530

    Update yolov8Transform.m

--- +helper/yolov8Transform.m
+++ +helper/yolov8Transform.m
@@ -7,28 +7,28 @@ outputsPerAnchor = numClasses + regMax*4;
 stride = [8,16,32];
 batch = 1;
 
-% Extract feature maps from Matlab model
-% Apply this if dlarray output
-predictions = cellfun(@extractdata,predictions,'UniformOutput',false);
-predictions = cellfun(@gather,predictions,'UniformOutput',false);
+pred = cell(3,1);
+predPermute = cell(3,1);
+predAll = cell(3,1);
+
+for i = 1:size(predictions,1)
+    pred{i,1} = permute(predictions{i,1},[2,1,3,4]);
+    predPermute{i,1} = reshape(pred{i,1},[],outputsPerAnchor,batch);
+    predAll{i,1} = permute(predPermute{i,1},[2,1,3,4]);
+end
+
+% Concat all Predictions
+predCat = cat(2,predAll{:});
+
+% Split classes and boxes
+box = predCat(1:64,:,:);
+cls = predCat(65:end,:,:);
 
 % Compute anchor grid and stride
 [anchorGrid, stride] = helper.make_anchors(predictions, stride);
 % anchor grid and stride transposed
 anchorGrid = anchorGrid';
 stride = stride';
-
-% Reshape predictions from model output
-pred = cellfun(@(p){permute(p,[2,1,3,4])}, predictions, 'UniformOutput',true);
-pred = cellfun(@(p){reshape(p,[],outputsPerAnchor, batch)}, pred, 'UniformOutput',true);
-pred = cellfun(@(p){permute(p,[2,1,3,4])}, pred, 'UniformOutput',true);
-
-% Concat all Predictions
-predCat = cat(2,pred{:});
-
-% Split classes and boxes
-box = predCat(1:64,:,:);
-cls = predCat(65:end,:,:);
 
 box = helper.distributionFocalLoss(box);
 % Converting boxes to xywh format here
